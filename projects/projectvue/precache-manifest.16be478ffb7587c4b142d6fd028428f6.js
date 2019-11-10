4560c84afd8e267dbbd454dd036b85ec50fe341d
patizhou <55703269+patizhou@users.noreply.github.com>
2019-11-10 17:18:33 +0800

    Add files via upload

---
+++ projects/projectvue/precache-manifest.16be478ffb7587c4b142d6fd028428f6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed44f1b221e5594d452e",
+    "url": "/css/app.c756e187.css"
+  },
+  {
+    "revision": "6f996bcbdfe359a9601c",
+    "url": "/css/chunk-vendors.297b0bba.css"
+  },
+  {
+    "revision": "d50cdf1e6fe93314ecea0b74cbf99540",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bc3e6f503d980c7da848",
+    "url": "/js/about.b0242c2d.js"
+  },
+  {
+    "revision": "ed44f1b221e5594d452e",
+    "url": "/js/app.cfae6681.js"
+  },
+  {
+    "revision": "6f996bcbdfe359a9601c",
+    "url": "/js/chunk-vendors.46eae13b.js"
+  },
+  {
+    "revision": "3056bfef67873caad29edfb6a005eb1e",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
